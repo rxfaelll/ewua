4ced1888e22937f175ae9acabf0e70e60cc6dccd
Rxfaelll <rafa.081299@gmail.com>
2019-11-26 20:47:42 -0300

    ARRUMEI O BUG DO SLIDE DA HOME (!!!!!!!!)

--- js/home.js
+++ js/home.js
@@ -1,9 +1,7 @@
-// DESISTO DE FAZER ESSA MERDA RODAR VAI TOMA NO CU
-
-// $(document).ready(function(){
-//     $(".slider-main").slick({
-//         autoplay: false,
-//         arrows: false,
-//         dots: true
-//     });
-// });
+$(document).ready(function(){
+    $(".slider-main").slick({
+        autoplay: false,
+        arrows: false,
+        dots: true
+    });
+});
